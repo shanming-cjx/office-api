8c1e745e293d518c31276cdcd580191afac549d5
Your Name <email@example.com>
2022-08-13 22:07:27 +0800

    2022-8-13 创建线程池配置类，开启多线程

--- src/main/java/com/chenjx/office/api/OfficeApiApplication.java
+++ src/main/java/com/chenjx/office/api/OfficeApiApplication.java
@@ -1,13 +1,19 @@
 package com.chenjx.office.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ComponentScan({"com.chenjx"})//扫描指定包路劲
 @MapperScan({"com.chenjx.office.api.mapper"})
+@ServletComponentScan
+@Slf4j
+@EnableAsync//开启多线程
 public class OfficeApiApplication {
 
 	public static void main(String[] args) {
