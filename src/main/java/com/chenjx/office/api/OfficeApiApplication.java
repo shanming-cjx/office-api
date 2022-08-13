b744523af6765d320c885d31887b0145504f3022
Your Name <email@example.com>
2022-08-13 12:17:08 +0800

    2022-8-12 连接MongoDB权限认证：密码必须使用单引号（字符串） 如：'password'

--- src/main/java/com/chenjx/office/api/OfficeApiApplication.java
+++ src/main/java/com/chenjx/office/api/OfficeApiApplication.java
@@ -1,9 +1,13 @@
 package com.chenjx.office.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.chenjx"})//扫描指定包路劲
+@MapperScan({"com.chenjx.office.api.mapper"})
 public class OfficeApiApplication {
 
 	public static void main(String[] args) {
