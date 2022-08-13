a2fe0dfbc0b1d13b8cb4a475ddfe8ed49c54c7b3
Your Name <email@example.com>
2022-08-13 13:41:23 +0800

    2022-8-13 使用SpringDoc代替Swagger，设置yml和SpringDocConfig配置类，并测试

---
+++ src/main/java/com/chenjx/office/api/controller/DocTestController.java
@@ -0,0 +1,23 @@
+package com.chenjx.office.api.controller;
+
+import com.chenjx.office.api.common.util.Resp;
+import io.swagger.v3.oas.annotations.Operation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+
+@RestController
+@RequestMapping("/user")
+@Tag(name = "DocTestController",description = "用户Web接口")
+public class DocTestController {//访问localhost:8090/office-api/swagger-ui.html【即springboot地址端口/项目名/swagger-ui.html（yml配置）】
+    @PostMapping("/checkCode")
+    @Operation(summary = "检测登陆验证码")
+    public Resp checkCode(@Valid String form) {
+        return Resp.ok().put("result", true);
+    }
+}
