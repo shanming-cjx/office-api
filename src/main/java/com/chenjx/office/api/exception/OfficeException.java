64d98324b6bc48f3eddd263c1422d06fc34899bb
Your Name <email@example.com>
2022-08-13 21:57:14 +0800

    2022-8-13 自定义异常与全局异常处理

---
+++ src/main/java/com/chenjx/office/api/exception/OfficeException.java
@@ -0,0 +1,34 @@
+package com.chenjx.office.api.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class OfficeException extends RuntimeException {
+    private String msg;
+    private int code = 500;
+
+    public OfficeException(String msg) {
+        super(msg);
+        this.msg = msg;
+    }
+
+    public OfficeException(String msg, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+    }
+
+    public OfficeException(String msg, int code) {
+        super(msg);
+        this.msg = msg;
+        this.code = code;
+    }
+
+    public OfficeException(String msg, int code, Throwable e) {
+        super(msg, e);
+        this.msg = msg;
+        this.code = code;
+    }
+
+}
