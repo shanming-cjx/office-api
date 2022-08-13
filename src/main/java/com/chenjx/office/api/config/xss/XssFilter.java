2bf9ba187eb3dc9a9d436b7f5a704ade4080ac41
Your Name <email@example.com>
2022-08-13 22:15:25 +0800

    2022-8-13 抵御XSS，实现XSS内容转义，XssFilter拦截http请求，调XssHttpServletRequestWrapper进行转义

---
+++ src/main/java/com/chenjx/office/api/config/xss/XssFilter.java
@@ -0,0 +1,28 @@
+package com.chenjx.office.api.config.xss;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+/**
+ *拦截所有的HTTP请求，然后调用XssHttpServletRequestWrapper
+ */
+@WebFilter(urlPatterns = "/*")
+public class XssFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request= (HttpServletRequest) servletRequest;
+        XssHttpServletRequestWrapper wrapper=new XssHttpServletRequestWrapper(request);
+        filterChain.doFilter(wrapper,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
