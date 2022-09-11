667368e628653afee87362df547f8d09aa7587b6
Your Name <email@example.com>
2022-09-12 07:32:15 +0800

    2022-9-12 对放行接口在自定义的JWT过滤器中放行

--- src/main/java/com/chenjx/office/api/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/chenjx/office/api/filter/JwtAuthenticationTokenFilter.java
@@ -33,7 +33,10 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //获取token
         String token = request.getHeader("token");
-        if (!StringUtils.hasText(token) || "null".equals(token)) {
+        //获取访问路径确认是否为放行路径
+        String servletPath = request.getServletPath();
+
+        if (!StringUtils.hasText(token) || "null".equals(token) || "/user/login".equals(servletPath)) {
             //放行
             filterChain.doFilter(request, response);
             return;
